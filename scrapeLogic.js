be707a60a5590d7ee170fb055eb88db63c0aa9cc
MΛCHINΣ CODΣ <ask@ragu.cz>
2024-04-20 15:27:35 +0200

    working version

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,8 +1,16 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch({
+const config = {
+  baseUrl: "https://aukro.cz/videokamery",
+  userAgent:
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+  nextPageSelector:
+    "auk-listing-items-list > div > auk-pagination-control > a.page-number.next > i",
+  itemSelector:
+    "a.item-card-main-container > div.item-card-body-wrapper > div.item-card-body-wrapper-top-container > auk-basic-item-card-title > h2",
+  launchOptions: {
+    headless: true,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -13,34 +21,102 @@ const scrapeLogic = async (res) => {
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
-  });
+  },
+  viewport: {
+    width: 1920,
+    height: 1080,
+  },
+};
+
+async function launchBrowser() {
+  return puppeteer.launch(config.launchOptions);
+}
+
+async function scrapeLogic(res) {
+  console.time("ScrapeLogicExecutionTime");
+  const browser = await launchBrowser();
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://simplecode.cz");
+    // Set user agent and viewport size
+    await page.setUserAgent(config.userAgent);
+    await page.setViewport(config.viewport);
 
-    // Wait for h1 element to appear
-    await page.waitForSelector("h1");
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
 
-    // Get the text content of the h1 element
-    const h1Text = await page.evaluate(() => {
-      const h1Element = document.querySelector("h1");
-      return h1Element ? h1Element.textContent.trim() : null;
-    });
-
-    if (h1Text) {
-      const logStatement = `The h1 text of the page is: ${h1Text}`;
-      console.log(logStatement);
-      res.send(logStatement);
-    } else {
-      res.send("No h1 element found on the page.");
-    }
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
+    const allData = await scrapePages(page);
+    res.write(JSON.stringify(allData, null, 2));
+    res.end();
+  } catch (error) {
+    console.error("Error during scraping: ", error);
+    res.send(
+      JSON.stringify({ error: `Při běhu Puppeteer došlo k chybě: ${error}` })
+    );
+    res.end();
   } finally {
     await browser.close();
+    console.timeEnd("ScrapeLogicExecutionTime");
   }
-};
+}
+
+async function scrapePages(page) {
+  let currentPage = 1;
+  let url = `${config.baseUrl}?size=180`;
+  let allData = [];
+
+  while (true) {
+    console.log(`Navigating to ${url}...`);
+    await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000 });
+    await autoScroll(page);
+
+    const h2Texts = await page.$$eval(config.itemSelector, (elements) =>
+      elements.map((element) => element.textContent.trim())
+    );
+    allData.push(...h2Texts.map((title) => ({ title })));
+    console.log(`${h2Texts.length} titles scraped from page ${currentPage}`);
+
+    const nextPage = await navigateToNextPage(page);
+    if (!nextPage) {
+      console.log("DONE: Reached the last page.");
+      break;
+    }
+
+    currentPage++;
+    url = `${config.baseUrl}?page=${currentPage}&size=180`;
+  }
+
+  return allData;
+}
+
+async function navigateToNextPage(page) {
+  const nextPageButton = await page.$(config.nextPageSelector);
+  if (nextPageButton) {
+    await nextPageButton.evaluate((b) => b.scrollIntoView());
+    await nextPageButton.click();
+    return page
+      .waitForNavigation({ waitUntil: "networkidle2" })
+      .then(() => true)
+      .catch(() => false);
+  }
+  return false;
+}
+
+async function autoScroll(page) {
+  await page.evaluate(
+    () =>
+      new Promise((resolve) => {
+        let totalHeight = 0;
+        const distance = 200;
+        const timer = setInterval(() => {
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+          if (totalHeight >= document.body.scrollHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, 50);
+      })
+  );
+}
 
 module.exports = { scrapeLogic };
