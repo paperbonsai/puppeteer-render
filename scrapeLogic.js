1c5e19a41dbae53f6ed49a7c04a4148e65725362
MΛCHINΣ CODΣ <ask@ragu.cz>
2024-04-20 21:25:48 +0200

    aasdasd

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const config = {
-  baseUrl: "https://aukro.cz/pc-graficke-karty",
+  baseUrl: "https://aukro.cz/rc-modely-drony",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
   nextPageSelector:
